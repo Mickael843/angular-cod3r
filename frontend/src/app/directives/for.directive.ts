c3e004ce796f2d3e5deb5f61e8cfb959c5e2519c
Mickael Luiz <mickaelluiz843@gmail.com>
2020-10-19 19:28:25 -0300

    segunda tentativa

---
+++ frontend/src/app/directives/for.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, Input, OnChanges, OnInit, SimpleChanges, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[myFor]'
+})
+export class ForDirective implements OnChanges {
+
+  @Input('myForEm') numbers: number[];
+
+  constructor(private container: ViewContainerRef, private template: TemplateRef<any>) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    for(let number of this.numbers) {
+      this.container.createEmbeddedView(this.template, { $implicit: number })
+    }
+  }
+
+}
